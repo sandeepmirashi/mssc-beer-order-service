e9772d9f3411a49ced76e0548a03ed3eb8f07fba
sandeepmirashi <sandeep.sm83@gmail.com>
2025-05-08 20:31:10 +0530

    CI problem .

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
-    public final static String VALIDATE_ORDER_QUEUE = 'validate-order';
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
 
     @Bean
     public MessageConverter jacksonJmsMessageConvertor(){
