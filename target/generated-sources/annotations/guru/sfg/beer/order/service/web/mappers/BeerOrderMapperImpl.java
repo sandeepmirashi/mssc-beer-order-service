f47f7165c751af970d7cbe54120e44d0553ff7fa
sandeepmirashi <sandeep.sm83@gmail.com>
2025-05-08 18:29:18 +0530

    Setup state machine transitions

--- target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/order/service/web/mappers/BeerOrderMapperImpl.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-05-08T17:27:52+0530",
+    date = "2025-05-08T17:29:32+0530",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.17 (Oracle Corporation)"
 )
 @Component
